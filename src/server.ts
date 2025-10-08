01a99c7ed8729fc15160bc0ed17b7c662262a023
Aymerick <juunn91@gmail.com>
2025-10-08 15:43:09 +0200

    Ajout de la base du front TypeScript avec NodeJS et Express page d'accueil

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+// Fichiers statiques (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes API
 app.use("/", routes);
 
 app.listen(PORT, () => {
